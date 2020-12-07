bd7fe79642c9f8f57ed72faff2f1e480b10a8d50
B. Lim <blim.digital@gmail.com>
2020-12-07 00:28:19 -0500

    added NewListActivity & NewItemActivity

---
+++ app/src/main/java/edu/towson/cosc435/buyr/NewListActivity.kt
@@ -0,0 +1,12 @@
+package edu.towson.cosc435.buyr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import edu.towson.cosc435.buyr.R
+
+class NewListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_list)
+    }
+}
